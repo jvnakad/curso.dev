6c7c1d0d3e84152663a161e33fe8ffce59bbf21b
jvnakad <112445763+jvnakad@users.noreply.github.com>
2025-04-26 00:41:38 +0000

    corrige arquivo index

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,4 @@ function Home() {
   return <h2>t amo biguuuuchaaaa s2</h2>;
 }
 
-function teste() {
-  console.log("teste");
-}
-
-function teste() {
-  console.log("teste");
-}
-
 export default Home;
