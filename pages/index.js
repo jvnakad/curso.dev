31e3ea1684411137918d15f27e11a5ec19b100b8
jvnakad <112445763+jvnakad@users.noreply.github.com>
2025-04-27 19:55:11 +0000

    Alteração na index

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
 function Home() {
-  return <h2>|Em construção</h2>;
+  function falar() {
+    alert('Você é bobucha e tem cheiro...');
+  }
+
+  return (
+    <div>
+      <h2>Em construção</h2>
+      <button onClick={falar}>Clique aqui!</button>
+    </div>
+  );
 }
 
 export default Home;
